7dd69a249ea6f4fe725f22858ee125d67dbe06a1
Castro <castrojonathand@gmail.com>
2023-07-27 14:58:07 -0300

    27/7

--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -9,7 +9,36 @@ import Card from '../Components/Card'
 const Home = () => {  
   
   const {dataState}= useGlobalContext()
-  const toggle = true
+  let toggle = true
+  const AddFav = ()=>{
+    // Aqui iria la logica para agregar la Card en el localStorage   
+    
+        console.log(toggle," apenas entra en AddFav [CARD]");       
+        
+        const item = { name, username, id, toggle }  
+        
+        console.log(item.toggle)
+
+        const favorites = JSON.parse(localStorage.getItem("favs")) || []
+        
+        const exists = favorites.some((favorite) => favorite.id === id)
+
+        if (!exists) {
+              
+          favorites.push(item);
+          toggle = true     
+          
+          localStorage.setItem("favs", JSON.stringify(favorites))          
+          console.log("favoritos", favorites) ;
+          console.log("Elemento agregado a favoritos:", item)
+        } else {
+          toggle = false 
+          const quitFav = favorites.filter(favoritos => favoritos.id !== id)
+          console.log("quitFav: ",quitFav)
+          localStorage.setItem("favs", JSON.stringify(quitFav))      
+          console.log("El elemento se quito de favoritos:", item)
+          } 
+      }
   return (
     <main className={dataState.theme ? 'light':'dark'}>
       <h1 style={{marginBottom:0}}>Home</h1>
@@ -18,7 +47,7 @@ const Home = () => {
       <div className='card-grid'>         
         {/* Aqui deberias renderizar las cards */}        
         {dataState.listData.map(list => <div key = {list.id}>                
-          <Card name={list.name} username={list.username} id={list.id} toggle={toggle}/>
+          <Card name={list.name} username={list.username} id={list.id} toggle={toggle} AddFav={AddFav}/>
         </div> )}       
       </div>
     </main>
