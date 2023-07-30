0c95a934f59a83c79d157363e1c884391f77c0a5
Castro <castrojonathand@gmail.com>
2023-07-29 23:37:51 -0300

    Prueba de commit

--- src/Routes/Contact.jsx
+++ src/Routes/Contact.jsx
@@ -7,8 +7,8 @@ const Contact = () => {
   return (
     <div >
       <div>
-      <h2>Want to know more?</h2>
-      <p>Send us your questions and we will contact you</p>
+      <h2> Want to know more?</h2>
+      <p> Send us your questions and we will contact you</p>
       </div>
       <Form/>
     </div>
