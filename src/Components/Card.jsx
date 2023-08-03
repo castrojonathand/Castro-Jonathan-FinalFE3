e7cedd44c3614eb26ed6c812c8b193192559223c
Castro <castrojonathand@gmail.com>
2023-08-03 19:18:37 -0300

    Corriguiendo cambios

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -4,10 +4,40 @@ import { Link } from "react-router-dom";
 // import { useContextGlobal } from "./utils/global.context";
 
 
-const Card = ({ name, username, id, toggle, AddFav}) => {
+const Card = ({ name, username, id, toggle}) => {
   
 
   console.log("Apenas entra en CARD",toggle)
+
+  const AddFav = ()=>{
+    // Aqui iria la logica para agregar la Card en el localStorage   
+    
+        console.log(toggle," apenas entra en AddFav [CARD]");       
+        
+        const item = { name, username, id, toggle }  
+        
+        console.log(item.toggle)
+
+        const favorites = JSON.parse(localStorage.getItem("favs")) || []
+        
+        const exists = favorites.some((favorite) => favorite.id === id)
+
+        if (!exists) {
+              
+          favorites.push(item);
+          toggle = true     
+          
+          localStorage.setItem("favs", JSON.stringify(favorites))          
+          console.log("favoritos", favorites) ;
+          console.log("Elemento agregado a favoritos:", item)
+        } else {
+          toggle = false 
+          const quitFav = favorites.filter(favoritos => favoritos.id !== id)
+          console.log("quitFav: ",quitFav)
+          localStorage.setItem("favs", JSON.stringify(quitFav))      
+          console.log("El elemento se quito de favoritos:", item)
+          } 
+      }
   
       
   return (
@@ -23,7 +53,7 @@ const Card = ({ name, username, id, toggle, AddFav}) => {
               <h3>estado: {toggle}</h3>
           </div>
         </Link>
-        <button onClick={AddFav} className="favButton">
+        <button onClick={()=> AddFav()} className="favButton">
           {toggle ? "Add to favorites" : "Remove from favorites"}   
           </button>        
         {/* No debes olvidar que la Card a su vez servira como Link hacia la pagina de detalle */}          
--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -10,35 +10,7 @@ const Home = () => {
   
   const {dataState}= useGlobalContext()
   let toggle = true
-  const AddFav = ()=>{
-    // Aqui iria la logica para agregar la Card en el localStorage   
-    
-        console.log(toggle," apenas entra en AddFav [CARD]");       
-        
-        const item = { name, username, id, toggle }  
-        
-        console.log(item.toggle)
-
-        const favorites = JSON.parse(localStorage.getItem("favs")) || []
-        
-        const exists = favorites.some((favorite) => favorite.id === id)
-
-        if (!exists) {
-              
-          favorites.push(item);
-          toggle = true     
-          
-          localStorage.setItem("favs", JSON.stringify(favorites))          
-          console.log("favoritos", favorites) ;
-          console.log("Elemento agregado a favoritos:", item)
-        } else {
-          toggle = false 
-          const quitFav = favorites.filter(favoritos => favoritos.id !== id)
-          console.log("quitFav: ",quitFav)
-          localStorage.setItem("favs", JSON.stringify(quitFav))      
-          console.log("El elemento se quito de favoritos:", item)
-          } 
-      }
+  
   return (
     <main className={dataState.theme ? 'light':'dark'}>
       <h1 style={{marginBottom:0}}>Home</h1>
@@ -47,7 +19,7 @@ const Home = () => {
       <div className='card-grid'>         
         {/* Aqui deberias renderizar las cards */}        
         {dataState.listData.map(list => <div key = {list.id}>                
-          <Card name={list.name} username={list.username} id={list.id} toggle={toggle} AddFav={AddFav}/>
+          <Card name={list.name} username={list.username} id={list.id} toggle={toggle}/>
         </div> )}       
       </div>
     </main>
