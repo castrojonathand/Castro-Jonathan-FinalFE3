7c5b75844fa7750915cd3b5e675685bf391f6d69
Castro <castrojonathand@gmail.com>
2023-12-03 06:25:18 -0300

    Adding Pagination

--- src/Components/Pagination.jsx
+++ src/Components/Pagination.jsx
@@ -1,74 +1,115 @@
-import { useState } from "react"
 import { useGlobalContext } from "./utils/global.context";
 
-const Pagination = () => {
+export const Pagination = ({
+  currentPage,
+  cardsPerPage,
+  setCurrentPage,
+  totalCards,
+}) => {
+  const { dataState} = useGlobalContext();
+  const totalPages = Math.ceil(totalCards / cardsPerPage);
 
-    const { dataState } = useGlobalContext();
-    let [num, setNum] = useState(1)
-    const [cur, setCur] = useState(1)
+  console.log("totalCardsPAGINATION>>", totalCards);
+  console.log("cardsPerPagePAGINATION>>", cardsPerPage);
+  console.log("currentPagePAGINATION>>", currentPage);
 
-    const pages = [
-        { page: num },
-        { page: num + 1 },
-        { page: num + 2 },
-        { page: num + 3 },
-    ]
-    function Next ()
-    {
-        setNum(++num)
+  const maxPagesToShow = 1;
+
+  let startPage = Math.max(currentPage - maxPagesToShow, 1);
+
+  let endPage = Math.min(currentPage + maxPagesToShow, totalPages); 
+
+  if (totalPages > maxPagesToShow * 2) {
+    if (currentPage <= maxPagesToShow) {
+      endPage = maxPagesToShow * 2 + 1;
+    } else if (currentPage >= totalPages - maxPagesToShow) {
+      startPage = totalPages - maxPagesToShow * 2;
     }
-    function back ()
-    {
-        num > 1 && setNum(--num)
-    }
-    return (
-      <div id="pagination" className={dataState.theme ? "light" : "dark"}>
-        <div className="flex justify-center w-full mx-auto rounded-lg font-[Poppins] ">
+  }
+
+  const pageNumber = [];
+  for (let i = startPage; i <= endPage; i++) {
+    pageNumber.push(i);
+  }
+
+  function Next() {
+    setCurrentPage(currentPage + 1);
+  }
+
+  function Back() {
+    setCurrentPage(currentPage - 1);
+  }
+ console.log("pageNumber>>>", pageNumber);
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <div className="bg-white rounded-lg font-[Poppins] flex items-center">
+        <button
+          onClick={Back}
+          className={
+            dataState.theme
+              ? `h-12 border-2 border-r-0 border-primary px-4 rounded-l-lg hover:bg-primary hover:opacity-60 hover:text-white ${
+                  currentPage === 1 ? "cursor-not-allowed opacity-30" : ""
+                }`
+              : `h-12 border-2 border-r-0 border-black px-4 rounded-l-lg hover:bg-gray-600 hover:opacity-60 ${
+                  currentPage === 1 ? "border-black cursor-not-allowed" : ""
+                }`
+          }
+          disabled={currentPage === 1}
+        >
+          <svg className="w-4 h-4 fill-current" viewBox="0 0 20 20">
+            <path
+              d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+              clipRule="evenodd"
+              fillRule="evenodd"
+            ></path>
+          </svg>
+        </button>
+        {pageNumber.map((pg, i) => (
           <button
-            onClick={back}
-            className="h-12 border-2 border-r-0 border-primary
-                        px-4 rounded-l-lg hover:bg-blue-600 hover:text-white"
+            key={i}
+            onClick={() => setCurrentPage(pg)}
+            className={
+              dataState.theme
+                ? `h-12 border-2 border-r-0 border-primary w-12 hover:bg-primary hover:opacity-60 hover:text-white ${
+                    currentPage === pg && "bg-primary"
+                  }`
+                : `h-12 border-2 border-r-0 border-black w-12 hover:bg-gray-600 hover:opacity-60 ${
+                    currentPage === pg && "bg-black"
+                  }`
+            }
           >
-            <svg class="w-4 h-4 fill-current" viewBox="0 0 20 20">
-              <path
-                d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-                clip-rule="evenodd"
-                fill-rule="evenodd"
-              ></path>
-            </svg>
+            {pg}
           </button>
-          {pages.map((pg, i) => (
-            <button
-              key={i}
-              onClick={() => setCur(pg.page)}
-              className={`h-12 border-2 border-r-0 border-primary
-                        w-12 ${cur === pg.page && "bg-primary text-white"}`}
-            >
-              {pg.page}
-            </button>
-          ))}
-          <button
-            onClick={Next}
-            className="h-12 border-2 border-primary
-                        px-4 rounded-r-lg hover:bg-blue-600 hover:text-white"
-          >
-            <svg class="w-4 h-4 fill-current" viewBox="0 0 20 20">
-              <path
-                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                clip-rule="evenodd"
-                fill-rule="evenodd"
-              ></path>
-            </svg>
-          </button>
-        </div>
+        ))}
+
+        <button
+          onClick={Next}
+          className={
+            dataState.theme
+              ? `h-12 border-2 border-primary px-4 rounded-r-lg hover:bg-primary hover:opacity-60 hover:text-white ${
+                  currentPage >= totalPages
+                    ? "border-black cursor-not-allowed opacity-30 border-l-0"
+                    : ""
+                }`
+              : `h-12 border-2  px-4 rounded-r-lg border-black hover:bg-gray-600 hover:opacity-60 ${
+                  currentPage >= totalPages
+                    ? "border-black cursor-not-allowed border-l-2 hover:bg-black"
+                    : ""
+                }`
+          }
+          disabled={currentPage >= totalPages}
+        >
+          <svg className="w-4 h-4 fill-current" viewBox="0 0 20 20">
+            <path
+              d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+              clipRule="evenodd"
+              fillRule="evenodd"
+            ></path>
+          </svg>
+        </button>
+        <br />
       </div>
-    );
-}
-
-export default Pagination
-
-
-
-
-
-
+      {totalPages > 3 && <p className="h-12 mt-2">Total pages: {totalPages}</p>}
+    </div>
+  );
+};
--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,9 +1,24 @@
 import { useGlobalContext } from "../Components/utils/global.context";
 import Card from "../Components/Card";
-import Pagination from "../Components/Pagination";
+import { Pagination } from "../Components/Pagination";
+import { useState } from "react";
+// import { usePathname} from 'next/navigation'
 
 const Home = () => {
   const { dataState, dataDispatch } = useGlobalContext();
+  // const path = usePathname()
+
+  const CARDS_PER_PAGE = 3;
+  const totalCards = dataState.listData.length;
+  console.log("dataState.listData.length>>>", dataState.listData.length);
+  // eslint-disable-next-line no-unused-vars
+  const [cardsPerPage, setCardsPerPage] = useState(CARDS_PER_PAGE);
+  const [currentPage, setCurrentPage] = useState(1);
+  console.log("CardsPerPage>>>", cardsPerPage);
+  console.log("currentPage>>>", currentPage);
+  
+  const lastIndex = currentPage * cardsPerPage;
+  const firstIndex = lastIndex - cardsPerPage;
 
   const handleRemoveFav = (id) => {
     const updatedFavs = dataState.favs.filter((fav) => fav.id !== id);
@@ -12,27 +27,34 @@ const Home = () => {
   };
 
   return (
-    <main className={dataState.theme ? "light" : "dark"}>
+    <main className={ `h-screen ${dataState.theme ? "" : "dark"}`}>
       <div className="mt-[68px] py-4">
         <h3> (click on the card to see details)</h3>
 
         <div className="grid justify-center sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 h-auto max-w-7xl mx-auto text-center">
-          {dataState.listData.map((list) => (
-            <div
-              className="flex flex-col items-center mx-4 p-4"
-              key={list.id}
-            >
-              <Card
-                name={list.name}
-                username={list.username}
-                id={list.id}
-                favToggle={list.toggle}
-                onRemoveFav={() => handleRemoveFav}
-              />
-            </div>
-          ))}
+          {dataState.listData
+            .map((list) => (
+              <div
+                className="flex flex-col items-center mx-4 p-4"
+                key={list.id}
+              >
+                <Card
+                  name={list.name}
+                  username={list.username}
+                  id={list.id}
+                  favToggle={list.toggle}
+                  onRemoveFav={() => handleRemoveFav}
+                />
+              </div>
+            ))
+            .slice(firstIndex, lastIndex)}
         </div>
-        <Pagination />
+        <Pagination
+          currentPage={currentPage}
+          cardsPerPage={cardsPerPage}
+          setCurrentPage={setCurrentPage}
+          totalCards={totalCards}
+        />
       </div>
     </main>
   );
