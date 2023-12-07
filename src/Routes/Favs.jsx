1b12b93e504aaa0103c4346af6ffc1f62238e698
Castro <castrojonathand@gmail.com>
2023-12-07 17:32:18 -0300

    fix Favs

--- src/Routes/Favs.jsx
+++ src/Routes/Favs.jsx
@@ -16,12 +16,20 @@ const Favs = () => {
       localStorage.setItem("favs", JSON.stringify(updatedFavs));
       dataDispatch({ type: "ADD_FAV", payload: updatedFavs });
     };
+    console.log("dataState.favs>>",dataState.favs)
   return (
-    <>     
-        <div className="mt-[68px] py-4 h-[1390px]">
-          <h1>Dentists Favs</h1>          
-          <div className="grid justify-center sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 flex-wrap h-auto max-w-7xl mx-auto text-center">
-            {dataState.favs.map((fav) => (
+    <>
+      <div className="mt-[68px] py-4">
+        <h1 className="my-4 mx-auto sm:w-[774px] md:w-[px] lg:w-[1324px]">
+          Dentists Favs
+        </h1>
+        <div className="grid justify-center sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 flex-wrap h-auto max-w-7xl mx-auto text-center">
+          {dataState.favs.length === 0 ? (
+            <h1 className="my-4 mx-auto sm:w-[774px] md:w-[px] lg:w-[1324px]">
+              No hay favoritos 🤷‍♂️
+            </h1>
+          ) : (
+            dataState.favs.map((fav) => (
               <div className="flex flex-col items-center mx-4 p-4" key={fav.id}>
                 <Card
                   name={fav.name}
@@ -31,9 +39,10 @@ const Favs = () => {
                   onRemoveFav={() => handleRemoveFav(fav.id)}
                 />
               </div>
-            ))}
-          </div>
-        </div>      
+            ))
+          )}
+        </div>
+      </div>
     </>
   );
 }
