305cbb9c1f49ee6bf7d97e07016e2508acc4f340
Castro <castrojonathand@gmail.com>
2023-12-23 15:51:27 -0300

    fix styles

--- src/Routes/Contact.jsx
+++ src/Routes/Contact.jsx
@@ -3,8 +3,8 @@ import Form from '../Components/Form'
 
 const Contact = () => {
   return (
-    <div className="mt-24 py-6 mx-auto text-center">
-      <div>
+    <div className="mt-24 py-4 mx-auto text-center">
+      <div className='py-4'>
         <h2> Want to know more?</h2>
         <p> Send us your questions and we will contact you</p>
       </div>
